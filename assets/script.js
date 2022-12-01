65013d771b05eb084caeb56b2fd5b73371f422dd
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-01 10:02:52 -0800

    added comments around next steps, tried a while loop for runQuiz function, now developing if statement, still working on runQuiz

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,8 @@ var answerChoiceTwo = document.createElement("li");
 var answerChoiceThree = document.createElement("li");
 var answerChoiceFour = document.createElement("li");
 var answerChoiceBank = [answerChoiceOne, answerChoiceTwo, answerChoiceThree, answerChoiceFour];
+
+var questionCount = 0;
      
 
 var questions = [
@@ -48,15 +50,24 @@ function startQuiz() {
 }  
 
 function runQuiz () {
-    for (i=0; i < questions.length; i++) {
-        questionOnDisplay.textContent = questions[i].questionText;
-        for (j=0; j < answerChoiceBank.length; j++) {
-            answerChoiceBank[j].textContent = questions[i].answerText[j];
+    if (questionCount < questions.length) {
+        //increment questionCount with the event listener on the li's. 
+        questionOnDisplay.textContent = questions[questionCount].questionText;
+        for (var j = 0; j < answerChoiceBank.length; j++) {
+            answerChoiceBank[j].textContent = questions[questionCount].answerText[j];
             answerChoices.appendChild(answerChoiceBank[j]);
         }
-        console.log(questionOnDisplay);
-        console.log(answerChoices);
     }
+}
+    // for (var i=0; i < questions.length; i++) {
+    //     questionOnDisplay.textContent = questions[questionCount].questionText;
+    //     for (var j=0; j < answerChoiceBank.length; j++) {
+    //         answerChoiceBank[j].textContent = questions[i].answerText[j];
+    //         answerChoices.appendChild(answerChoiceBank[j]);
+    //     }
+    //     console.log(questionOnDisplay);
+    //     console.log(answerChoices);
+    // }
     //displayQuestions
     //displayAnswers
     //picks an answer with click
@@ -65,5 +76,8 @@ function runQuiz () {
     //display next answer etc. etc. 
     //this will run on a loop the length of the questions array.
     //endGame will be triggered upon this loop running out
-}
+
 runQuiz();
+
+//I need to change this for loop to a while loop so that this will run so long as a given condition is true. 
+//I will have a count var, and while count is < questions.array and timer < 0. At the end of the while loop I will increment it by one and so long as that count variable is remaining below the length of the questions array. We like the for loop for printing answers, we want the while loop for regulating the game pace/checking status of game 
