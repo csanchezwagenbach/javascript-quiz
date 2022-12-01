4d00b898cf092065a6264f2c7f19276c495b8d17
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-11-30 17:55:37 -0800

    added variables and arrays to hold information into js file, began working with objects to change html elements on page

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,59 @@
 var initialDisplay = document.querySelector("#initial-display");
 var questionsDisplay = document.querySelector("#questions-display");
+var questionOnDisplay = document.querySelector("#question");
+var answerChoices = document.querySelector("#answer-list");
+
+
+var answerChoiceOne = document.createElement("li");
+var answerChoiceTwo = document.createElement("li");
+var answerChoiceThree = document.createElement("li");
+var answerChoiceFour = document.createElement("li");
+var answerChoiceBank = [answerChoiceOne, answerChoiceTwo, answerChoiceThree, answerChoiceFour];
+     
+
+var questions = [
+    {
+        questionText: "Which of the following does not represent a primitive type?",
+        answerText:["Boolean","Number","Strand", "Undefined"],
+        correctAnswer:"Strand"
+    },
+    {
+        questionText:"How many possible states do Boolean values have?",
+        answerText:["One","Two","Three", "Four"],
+        correctAnswer: "Two"
+    },
+    {
+        questionText: "What does API stand for?",
+        answerText: ["Apple Pizza, Ingrid!", "All Purposes Ice", "Application Programming Interface", "Any Price Index"],
+        correctAnswer: "Application Programming Interface"
+    },
+    {
+        questionText: "Where is local storage?",
+        answerText: ["Around the corner", "Hiding inside your browser!", "Local storage? No such thing", "Hre, there, everywhere"]
+    }
+]
+
+
+    // questionOnDisplay.textContent = questions[i].questionText
+    // 
+
+questionOnDisplay.textContent = questions[0].questionText
+
+
 function startQuiz() {
     initialDisplay.classList.remove("start");
     initialDisplay.classList.add("hidden");
     questionsDisplay.classList.remove("hidden");
+    //startTimer
+}  
+
+function runQuiz () {
+    //displayQuestions
+    //displayAnswers
+    //picks an answer with click
+    //checkIfWrong 
+    //display next question
+    //display next answer etc. etc. 
+    //this will run on a loop the length of the questions array.
+    //endGame will be triggered upon this loop running out
 }
-
-
-let questions = [
-    {
-        q:'<your_question>',
-        a:['a','b','c', 'd'],
-        ca:'a'
-    },
-    {
-        q:'<your_question>',
-        a:['a','b','c', 'd'],
-        ca:'a'
-    },
-]
-
-console.log(questions[0].a)
